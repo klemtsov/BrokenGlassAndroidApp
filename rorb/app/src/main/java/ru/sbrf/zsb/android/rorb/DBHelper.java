31b2a4391c1626a4ed4129dfefd97a0f06c2f444
Anton <muzychenkoaa@gmail.com>
2016-06-17 16:01:42 +0600

    -- Create Uaser tbl

--- rorb/app/src/main/java/ru/sbrf/zsb/android/rorb/DBHelper.java
+++ rorb/app/src/main/java/ru/sbrf/zsb/android/rorb/DBHelper.java
@@ -15,7 +15,7 @@ import ru.sbrf.zsb.android.helper.Utils;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final int DB_VERSION = 2;
+    private static final int DB_VERSION = 5;
     private static final String DB_NAME = "brglass_db";
     private static final String CLAIME_TBL = "claime";
     public static final String ADDRESS_TBL = "address";
@@ -123,7 +123,7 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("drop table claime");
         db.execSQL("drop table photo");
         */
-        Log.d(MainActivity3.TAG, " --- UPDATE DATABASE --- ");
+/*        Log.d(MainActivity3.TAG, " --- UPDATE DATABASE --- ");
         db.execSQL("create table " + USER_TBL
                 + " (_id integer primary key autoincrement,"
                 + " email text NOT NULL,"
@@ -136,7 +136,7 @@ public class DBHelper extends SQLiteOpenHelper {
                 + " is_login integer default 0,"
                 + " CONSTRAINT cnstr_user_email UNIQUE (email)"
                 + ");"
-        );
+        );*/
     }
 
     public void openDB() {
